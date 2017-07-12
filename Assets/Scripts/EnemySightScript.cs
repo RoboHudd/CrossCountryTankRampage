1553d2c3563ca541f176c2389747901c099f7454
RoboHudd <robohudd@gmail.com>
2017-07-12 18:43:41 +0100

    Sight cone
    Added the sight cone that collides with the player, no proper collision response as of yet

---
+++ Assets/Scripts/EnemySightScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySightScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnCollisionEnter2D (Collision2D collision)
+    {
+        EnemyMovementScript moveScript = GetComponent<EnemyMovementScript>();
+        Debug.Log("Hit");
+        if (moveScript != null)
+        {
+            moveScript.speed += 1;
+            moveScript.turnSpeed += 50;
+            Debug.Log("Success");
+        }
+        else
+        {
+            Debug.Log("Null script");
+        }
+    }
+}
